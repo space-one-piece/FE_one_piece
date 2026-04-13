a8e5313f4f6800e64d168a4517c705e31695fd58
DESKTOP-BETG49P\A <fishwwww0516@naver.com>
2026-04-13 14:49:59 +0900

    feat: 아이콘 size variant 추가 (#36)

--- src/shared/components/empty/Empty.tsx
+++ src/shared/components/empty/Empty.tsx
@@ -2,14 +2,31 @@ import { cn } from "@/lib/utils"
 import { Image as ImageIcon, UserRound } from "lucide-react"
 
 type EmptyType = "avatar" | "image"
+type EmptySize = "sm" | "md"
 
 export type EmptyProps = {
-  type: EmptyType
+  type?: EmptyType
+  size?: EmptySize
   className?: string
 }
 
-// 기본값: image
-const Empty = ({ type = "image", className }: EmptyProps) => {
+const sizeStyle = {
+  avatar: {
+    sm: "h-[80px] w-[80px]",
+    md: "h-[124px] w-[124px]",
+  },
+  image: {
+    sm: "h-[100px] w-[100px]",
+    md: "h-[138px] w-[138px]",
+  },
+}
+
+const iconSize = {
+  sm: "h-6 w-6",
+  md: "h-9 w-9",
+}
+
+const Empty = ({ type = "image", size = "md", className }: EmptyProps) => {
   const isAvatar = type === "avatar"
 
   return (
@@ -17,19 +34,22 @@ const Empty = ({ type = "image", className }: EmptyProps) => {
       <div
         className={cn(
           "flex shrink-0 items-center justify-center border border-border",
+          sizeStyle[type][size],
           isAvatar
-            ? "h-[124px] w-[124px] rounded-full shadow-md bg-white"
-            : "h-[138px] w-[138px] rounded-lg bg-disabled"
+            ? "rounded-full shadow-md bg-white"
+            : "rounded-lg bg-disabled"
         )}
-        // TODO: 반응형 업데이트 필요
       >
         {isAvatar ? (
           <UserRound
-            className="h-10 w-10 text-text-primary"
+            className={cn(iconSize[size], "text-text-primary")}
             strokeWidth={2.2}
           />
         ) : (
-          <ImageIcon className="h-9 w-9 text-text-primary" strokeWidth={1.8} />
+          <ImageIcon
+            className={cn(iconSize[size], "text-text-primary")}
+            strokeWidth={1.8}
+          />
         )}
       </div>
     </div>
