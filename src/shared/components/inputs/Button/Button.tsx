009b62b7b8aed5aebdcf859fc998f70d2f63c53c
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-04-13 14:26:00 +0900

    feat: 버튼 초안 작성 (#33)

--- src/shared/components/inputs/Button/Button.tsx
+++ src/shared/components/inputs/Button/Button.tsx
@@ -1,13 +1,56 @@
 import type { DefaultButtonProps } from "@/shared/types"
+import { cva } from "class-variance-authority"
+import clsx from "clsx"
 import type { ReactNode } from "react"
 
+const buttonVariants = cva("", {
+  variants: {
+    size: {
+      sm: "text-sm py-lg py-xl",
+      lg: "text-md py-lg py-xl",
+    },
+    shape: {
+      rectangle: "rounded-md",
+      pill: "rounded-full",
+    },
+    isContained: {
+      false: "border border-primary hover:border-primary-hover",
+      true: "bg-primary hover:bg-primary text-text-button",
+    },
+    disabled: {
+      false: "border border-primary-disabled text-primary-disabled",
+      true: "cursor-pointer text-text-disabled",
+    },
+  },
+})
+
 type ButtonProps = {
-  isDisabled: boolean
-  children: ReactNode
+  size?: "sm" | "lg"
+  shape?: "rectangle" | "pill"
+  isContained?: boolean
+  disabled?: boolean
+  children?: ReactNode
 }
-const Button = ({ children }: DefaultButtonProps & ButtonProps) => {
-  // NOTE: inputs 폴더의 placeholder 용입니다
-  return <button>{children}</button>
+const Button = ({
+  size = "sm",
+  shape = "rectangle",
+  isContained = true,
+  disabled = false,
+  children,
+  ...props
+}: DefaultButtonProps & ButtonProps) => {
+  const { className, ...rest } = props
+  return (
+    <button
+      {...rest}
+      className={clsx(
+        buttonVariants({ size, shape, isContained, disabled }),
+        className
+      )}
+    >
+      {children}
+    </button>
+  )
 }
 
 export default Button
