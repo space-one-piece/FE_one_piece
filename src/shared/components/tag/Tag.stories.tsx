05a8a62b15cbf9b12e8290898f27b5ea8d0c225a
Clacki <skthk6613@naver.com>
2026-04-13 17:25:35 +0900

    feat: Tag 컴포넌트 구현 (#11)

---
+++ src/shared/components/tag/Tag.stories.tsx
@@ -0,0 +1,63 @@
+import type { Meta, StoryObj } from "@storybook/react-vite"
+import Tag from "./Tag"
+
+const meta = {
+  title: "Components/Tag",
+  component: Tag,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    label: {
+      control: "text",
+    },
+    size: {
+      control: "radio",
+      options: ["sm", "md"],
+    },
+    variant: {
+      control: "radio",
+      options: ["outlined", "selected", "soft", "subtle"],
+    },
+    onClick: {
+      action: "clicked",
+    },
+  },
+} satisfies Meta<typeof Tag>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    label: "산뜻한",
+    size: "sm",
+    variant: "outlined",
+  },
+}
+
+export const Selected: Story = {
+  args: {
+    label: "생기 있는",
+    size: "sm",
+    variant: "selected",
+  },
+}
+
+export const Soft: Story = {
+  args: {
+    label: "따뜻한",
+    size: "sm",
+    variant: "soft",
+  },
+}
+
+export const Subtle: Story = {
+  args: {
+    label: "베르가못",
+    size: "sm",
+    variant: "subtle",
+  },
+}
---
+++ src/shared/components/tag/Tag.tsx
@@ -0,0 +1,56 @@
+import { cva } from "class-variance-authority"
+import clsx from "clsx"
+
+type TagVariant = "outlined" | "selected" | "soft" | "subtle"
+type TagSize = "sm" | "md"
+
+type TagProps = {
+  label: string
+  size?: TagSize
+  variant?: TagVariant
+  onClick?: () => void
+}
+
+const tagVariants = cva(
+  "inline-flex items-center justify-center rounded-full transition-colors",
+  {
+    variants: {
+      size: {
+        sm: "text-sm py-sm px-md",
+        md: "text-md py-xs px-sm",
+      },
+      variant: {
+        outlined: "cursor-pointer border border-border text-text-sub bg-gray-0",
+        selected: "cursor-pointer bg-button text-text-button",
+        soft: "cursor-default bg-surface-container text-text-highlight",
+        subtle:
+          "cursor-default border border-border bg-surface-default text-text-sub",
+      },
+    },
+    defaultVariants: {
+      size: "sm",
+      variant: "outlined",
+    },
+  }
+)
+
+const Tag = ({ label, size, variant, onClick }: TagProps) => {
+  return (
+    <>
+      <button
+        type="button"
+        onClick={onClick}
+        className={clsx(
+          tagVariants({
+            size,
+            variant,
+          })
+        )}
+      >
+        {label}
+      </button>
+    </>
+  )
+}
+
+export default Tag
