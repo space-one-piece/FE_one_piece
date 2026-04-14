2ddb5259e58b651a19a3e538225573113ee90410
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-04-14 17:29:57 +0900

    feat: 패스워드 인풋 초안 작성 (#61)

---
+++ src/shared/components/inputs/password-input/PasswordInput.tsx
@@ -0,0 +1,36 @@
+import type { InputProps } from "@/shared/types"
+import { Eye, EyeOff } from "lucide-react"
+import { useState, type Dispatch, type SetStateAction } from "react"
+import Input from "../Input/Input"
+
+type EyeButtonProps = {
+  isHidden: boolean
+  setIsHidden: Dispatch<SetStateAction<boolean>>
+}
+const EyeButton = ({ isHidden, setIsHidden }: EyeButtonProps) => {
+  const handleClick = () => setIsHidden((prev) => !prev)
+  const Icon = isHidden ? <EyeOff /> : <Eye />
+  return <button onClick={handleClick}>{Icon}</button>
+}
+
+type WithPasswordInputProps = {
+  isError: boolean
+}
+const PasswordInput = ({
+  isError,
+  ...props
+}: Omit<InputProps, "type"> & WithPasswordInputProps) => {
+  const [isHidden, setIsHidden] = useState(true)
+  return (
+    <Input
+      {...props}
+      status={isError ? "error" : "none"}
+      type={isHidden && "password"}
+      trailingChild={
+        <EyeButton isHidden={isHidden} setIsHidden={setIsHidden} />
+      }
+    />
+  )
+}
+
+export default PasswordInput
