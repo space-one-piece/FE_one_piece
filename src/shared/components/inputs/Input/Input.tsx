2c7e587be0f62165c984b3aee604b226d4cc5055
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-04-14 13:45:03 +0900

    feat: 공통 인풋 초안 작성 (#40)

--- src/shared/components/inputs/Input/Input.tsx
+++ src/shared/components/inputs/Input/Input.tsx
@@ -1,5 +1,40 @@
-const Input = () => {
-  return <div></div>
+import type { InputProps } from "@/shared/types"
+import type { None } from "@/shared/types/commonPropsTypes/commonPropsTypes"
+import { cva } from "class-variance-authority"
+import clsx from "clsx"
+import type { ReactNode } from "react"
+import { Hstack } from "../../layouts"
+
+const inputVariants = cva(
+  "rounded-sm transition items-center outline flex-1 bg-card text-text-primary",
+  {
+    variants: {
+      status: {
+        none: "",
+        success: "outline-border-primary",
+        error: "outline-border-toast-error",
+      },
+    },
+  }
+)
+
+type WithInputProps = {
+  status?: None | "success" | "error"
+  trailingChild?: ReactNode
+}
+const Input = ({
+  status = "none",
+  trailingChild,
+  ...props
+}: InputProps & WithInputProps) => {
+  const { className, ...rest } = props
+
+  return (
+    <Hstack>
+      <Input {...rest} className={clsx(inputVariants({ status }), className)} />
+      {trailingChild}
+    </Hstack>
+  )
 }
 
 export default Input
