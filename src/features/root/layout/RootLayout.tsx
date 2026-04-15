b27e8b24b83f5bafc50e961731619203ffac2e41
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-04-15 16:23:51 +0900

    fix: 루트 레이아웃 병합 충돌 해결 (#59)

--- src/features/root/layout/RootLayout.tsx
+++ src/features/root/layout/RootLayout.tsx
@@ -3,11 +3,10 @@ import { Outlet } from "@tanstack/react-router"
 
 const RootLayout = () => {
   return (
-    <div>
-      <div>this is header placeholder</div>
+    <>
       <Outlet />
       <Toaster />
-    </div>
+    </>
   )
 }
 
