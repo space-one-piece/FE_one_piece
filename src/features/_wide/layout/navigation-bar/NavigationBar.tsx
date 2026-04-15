519ce01939099eb86b8a09f1d93423c45abd8973
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-04-15 15:48:24 +0900

    feat: 다른 버튼들도 제작 (#59)

--- src/features/_wide/layout/navigation-bar/NavigationBar.tsx
+++ src/features/_wide/layout/navigation-bar/NavigationBar.tsx
@@ -1,40 +1,57 @@
 import { Hstack, Vstack } from "@/shared/components"
 import { useLocation, useNavigate } from "@tanstack/react-router"
 import clsx from "clsx"
-import { Home } from "lucide-react"
+import { Home, Scroll, Sparkles, User } from "lucide-react"
 
-const HomeButton = () => {
-  const pathname = useLocation({
+type NavigationPathname = "/" | "/list" | "/search" | "/my-page"
+type LucideIconProps = typeof Home
+
+type NavigationButtonConfig = {
+  pathname: NavigationPathname
+  label: string
+  Icon: LucideIconProps
+}
+
+const navigationButtonConfigs: NavigationButtonConfig[] = [
+  { pathname: "/", label: "Home", Icon: Home },
+  { pathname: "/list", label: "Home", Icon: Scroll },
+  { pathname: "/search", label: "Home", Icon: Sparkles },
+  { pathname: "/my-page", label: "Home", Icon: User },
+]
+
+const NavigationButton = ({
+  pathname,
+  label,
+  Icon,
+}: NavigationButtonConfig) => {
+  const currentPathname = useLocation({
     select: (location) => location.pathname,
   })
-  const isSelected = pathname === "/"
+  const isSelected = currentPathname === pathname
   const navigate = useNavigate()
   return (
     <button>
       <Vstack gap="none">
-        <Home
+        <Icon
           size={40}
           className={clsx(
             "rounded-md transition",
             isSelected ? "text-card bg-primary scale-120" : "text-primary"
           )}
-          onClick={() => navigate({ to: "/" })}
+          onClick={() => navigate({ to: pathname })}
         />
-        <p>Home</p>
+        <p>{label}</p>
       </Vstack>
     </button>
   )
 }
 
 const NavigationBar = () => {
-  const navigate = useNavigate()
-
   return (
     <Hstack className="justify-evenly">
-      <HomeButton />
-      <button onClick={() => navigate({ to: "/not-real" })}>
-        move to not home
-      </button>
+      {navigationButtonConfigs.map((config) => (
+        <NavigationButton {...config} />
+      ))}
     </Hstack>
   )
 }
