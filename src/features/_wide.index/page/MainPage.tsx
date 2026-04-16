890e41d0e98656c7f050a8323838378aeab946f8
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-04-16 14:18:03 +0900

    feat: 폴더 및 파일 생성 (#83)

--- src/features/_wide.index/page/MainPage.tsx
+++ src/features/_wide.index/page/MainPage.tsx
@@ -1,15 +1,21 @@
-import { Button } from "@/shared/components"
-import { useNavigate } from "@tanstack/react-router"
+import { Button, Vstack } from "@/shared/components"
 
 const MainPage = () => {
-  const navigate = useNavigate()
   return (
-    <div>
-      <p>this is main page placeholder</p>
-      <Button onClick={() => navigate({ to: "/not-real" })}>
-        navigate to "not-real" page
-      </Button>
-    </div>
+    <Vstack gap="2xl">
+      <div className="bg-amber-50">
+        <Vstack className="h-125 items-center p-2xl justify-between">
+          <Vstack className="justify-start w-full">
+            <h1>내 공간을 완성해줄 마지막 한 조각</h1>
+            <p>가장 편안한 공간의 사진 한 장,</p>
+            <p>혹은 지금의 기분만 들려주세요.</p>
+            <p>당신의 취향 조각들을 모아</p>
+            <p>공간을 완벽하게 마무리 해줄 향기를 찾아낼게요.</p>
+          </Vstack>
+          <Button>나의 마지막 조각 찾기</Button>
+        </Vstack>
+      </div>
+    </Vstack>
   )
 }
 
