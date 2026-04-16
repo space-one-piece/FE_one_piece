4e7d698323b5722d9dda0c9d3514f112a1f2e084
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-04-16 16:55:45 +0900

    design: quickstart card container의 너비 최대로 수정 (#93)

--- src/features/_wide.index/page/4-quick-start/QuickStart.tsx
+++ src/features/_wide.index/page/4-quick-start/QuickStart.tsx
@@ -8,7 +8,8 @@ const QuickStart = () => {
       <h2>간편한 시작</h2>
       <h3>fragmnt 이렇게 사용해보세요</h3>
       <p>세 번의 과정으로 당신의 공간에 딱 맞는 향기를 찾아보세요.</p>
-      <Hstack gap="xl">
+
+      <Hstack gap="xl" className="w-full">
         <RoundBox radius="lg" className="bg-amber-200 w-full">
           card content placeholder
         </RoundBox>
@@ -19,6 +20,7 @@ const QuickStart = () => {
           card content placeholder
         </RoundBox>
       </Hstack>
+
       <Hstack gap="none">
         <Button isContained={false}>Start now</Button>
         <Button>
