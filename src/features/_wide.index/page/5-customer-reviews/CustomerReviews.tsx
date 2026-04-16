721b4491e29f2121cb3fbf55c040cc30351bf19c
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-04-16 15:46:52 +0900

    feat: 리뷰 페이지 레이아웃 제작 (#93)

--- src/features/_wide.index/page/5-customer-reviews/CustomerReviews.tsx
+++ src/features/_wide.index/page/5-customer-reviews/CustomerReviews.tsx
@@ -1,5 +1,37 @@
+import { Hstack, RoundBox } from "@/shared/components"
+import { ChevronLeft, ChevronRight } from "lucide-react"
+import SectionVstack from "../section-container/SectionContainer"
+
+const ReviewCard = () => {
+  return (
+    <RoundBox padding="none" className="w-60 h-90 shrink-0 bg-amber-100">
+      review card placeholder
+    </RoundBox>
+  )
+}
+
+// TODO: RoundButton 공통 컴포넌트 만들어야(기존 button 확장하든, 새로 만들든)
 const CustomerReviews = () => {
-  return <div>this is customer reviews</div>
+  return (
+    <SectionVstack>
+      <h2>고객 후기</h2>
+      <Hstack className="justify-end">
+        <button>
+          <ChevronLeft />
+        </button>
+        <button>
+          <ChevronRight />
+        </button>
+      </Hstack>
+      <Hstack className="overflow-hidden">
+        {Array(10)
+          .fill(0)
+          .map((_, index) => (
+            <ReviewCard key={index} />
+          ))}
+      </Hstack>
+    </SectionVstack>
+  )
 }
 
 export default CustomerReviews
