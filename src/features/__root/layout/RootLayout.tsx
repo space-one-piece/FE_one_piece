64ca6ed9ada4ecf52e12ecb2862ac063fdbaf595
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-04-17 14:47:23 +0900

    chore: temp (#103)

--- src/features/__root/layout/RootLayout.tsx
+++ src/features/__root/layout/RootLayout.tsx
@@ -1,12 +1,22 @@
+import { FullScreen } from "@/shared/components"
 import { Toaster } from "@/shared/components/toast"
 import { Outlet } from "@tanstack/react-router"
 
+// const selectImage = (pathname: string) => {
+//   if (pathname.includes("auth")) return BgAuthSrc
+//   if (pathname.includes("find-scent")) return BgFindScent
+//   if (pathname.includes("")) return BgMypage
+//   return BgMainSrc
+// }
+
 const RootLayout = () => {
+  // const pathname = useLocation().pathname
+
   return (
-    <>
+    <FullScreen>
       <Outlet />
       <Toaster />
-    </>
+    </FullScreen>
   )
 }
 
