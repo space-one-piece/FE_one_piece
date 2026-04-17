6a9d1730bf463ae2e850421f22b79e3a39411dc0
Clacki <skthk6613@naver.com>
2026-04-17 16:16:38 +0900

    fix: storybook 수정 반영 (#106)

--- src/features/scent-survey/page/preference-slider/PreferenceSlider.stories.tsx
+++ src/features/scent-survey/page/preference-slider/PreferenceSlider.stories.tsx
@@ -17,6 +17,9 @@ const meta = {
     ),
   ],
   argTypes: {
+    item: {
+      control: "object",
+    },
     value: {
       control: {
         type: "range",
@@ -40,17 +43,19 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
   args: {
-    order: 1,
-    title: "아침에 선호하는 향",
-    description: "하루를 시작할 때 어떤 느낌의 향을 원하시나요?",
-    labels: [
-      "매우 상쾌함",
-      "약간 상쾌함",
-      "보통",
-      "약간 포근함",
-      "매우 포근함",
-    ],
-    edgeLabels: ["상쾌한", "포근한"],
+    item: {
+      order: 1,
+      title: "아침에 선호하는 향",
+      description: "하루를 시작할 때 어떤 느낌의 향을 원하시나요?",
+      labels: [
+        "매우 상쾌함",
+        "약간 상쾌함",
+        "보통",
+        "약간 포근함",
+        "매우 포근함",
+      ],
+      edgeLabels: ["상쾌한", "포근한"],
+    },
     value: 2,
   },
   render: function Render(args) {
