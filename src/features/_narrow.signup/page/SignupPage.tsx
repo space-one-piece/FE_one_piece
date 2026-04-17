65e9df61a60000da3eae35e11d1f17f3f15b85f6
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-04-18 01:24:01 +0900

    design: 맨 밑 버튼 마진 조절 (#122)

--- src/features/_narrow.signup/page/SignupPage.tsx
+++ src/features/_narrow.signup/page/SignupPage.tsx
@@ -38,7 +38,7 @@ const SignupPage = () => {
           <Labeled.Title>생년월일</Labeled.Title>
           <Input placeholder="8자리 숫자 입력 (예: 19980514)" />
         </Labeled>
-        <Button>회원가입</Button>
+        <Button className="mt-lg">회원가입</Button>
       </Vstack>
     </form>
   )
