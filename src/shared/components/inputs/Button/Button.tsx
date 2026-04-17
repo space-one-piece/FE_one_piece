2db73b53584e3619bf454070f62b9885e79fb00a
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-04-17 13:41:22 +0900

    design: ghost 디자인 추가 (#99)

--- src/shared/components/inputs/Button/Button.tsx
+++ src/shared/components/inputs/Button/Button.tsx
@@ -1,27 +1,37 @@
+import cn from "@/lib/utils"
 import type { DefaultButtonProps } from "@/shared/types"
 import { cva } from "class-variance-authority"
 import clsx from "clsx"
 import type { ReactNode } from "react"
+import { Hstack } from "../../layouts"
 
 const buttonVariants = cva("transition", {
   variants: {
+    padding: {
+      wide: "",
+      same: "",
+    },
+    radius: {
+      md: "rounded-md",
+      full: "rounded-full",
+    },
     size: {
-      sm: "text-md py-sm px-xl",
-      lg: "text-md py-lg px-xl",
+      sm: "text-md", // NOTE: 글자 크기도 다르게 한다면 수정
+      lg: "text-md", // NOTE: 글자 크기도 다르게 한다면 수정
     },
-    shape: {
-      rectangle: "rounded-md",
-      pill: "rounded-full",
-    },
-    isContained: {
-      false: clsx(
+    style: {
+      contained: clsx(
+        "bg-primary hover:bg-primary-hover disabled:bg-primary-disabled",
+        "text-text-button disabled:text-text-disabled"
+      ),
+      outlined: clsx(
         "border border-primary hover:border-primary-hover disabled:border-primary-disabled",
         "bg-card hover:bg-gray-5",
         "text-primary disabled:text-text-disabled"
       ),
-      true: clsx(
-        "bg-primary hover:bg-primary-hover disabled:bg-primary-disabled",
-        "text-text-button disabled:text-text-disabled"
+      ghost: clsx(
+        "hover:bg-gray-5",
+        "text-primary disabled:text-text-disabled"
       ),
     },
     disabled: {
@@ -29,37 +39,45 @@ const buttonVariants = cva("transition", {
       true: "",
     },
   },
+  compoundVariants: [
+    { padding: "wide", size: "sm", className: "py-sm px-xl" },
+    { padding: "wide", size: "lg", className: "py-lg px-xl" },
+    { padding: "same", size: "sm", className: "p-sm" },
+    { padding: "same", size: "lg", className: "p-lg" },
+  ],
 })
 
 type ButtonProps = {
   size?: "sm" | "lg"
-  shape?: "rectangle" | "pill"
-  isContained?: boolean
+  padding?: "wide" | "same"
+  radius?: "md" | "full"
+  style?: "contained" | "outlined" | "ghost"
   disabled?: boolean
   children?: ReactNode
 }
 const Button = ({
   size = "sm",
-  shape = "rectangle",
-  isContained = true,
+  padding = "wide",
+  radius = "md",
+  style = "contained",
   disabled = false,
   children,
   ...props
 }: DefaultButtonProps & ButtonProps) => {
   const { className, ...rest } = props
   return (
-    <>
-      <button
-        {...rest}
-        disabled={disabled}
-        className={clsx(
-          buttonVariants({ size, shape, isContained, disabled }),
-          className
-        )}
-      >
+    <button
+      {...rest}
+      disabled={disabled}
+      className={cn(
+        buttonVariants({ size, padding, radius, style, disabled }),
+        className
+      )}
+    >
+      <Hstack gap="sm" className="items-center">
         {children}
-      </button>
-    </>
+      </Hstack>
+    </button>
   )
 }
 
