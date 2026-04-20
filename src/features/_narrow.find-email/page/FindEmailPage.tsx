32a771d7af3e0998e6e134ae1226ed653fb13d02
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-04-20 19:37:18 +0900

    feat: 이메일 찾기 페이지 ui 제작 (#145)

--- src/features/_narrow.find-email/page/FindEmailPage.tsx
+++ src/features/_narrow.find-email/page/FindEmailPage.tsx
@@ -1,5 +1,88 @@
+import NarrowTitleSection from "@/features/_narrow/components/narrow-title-section/NarrowTitleSection"
+import { plainInstance } from "@/shared/api/axios-instance"
+import { Button, Hstack, Input, Vstack } from "@/shared/components"
+import Labeled from "@/shared/components/inputs/labeled/Labeled"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useMutation } from "@tanstack/react-query"
+import { useForm } from "react-hook-form"
+import z from "zod"
+
+const findEmailSchema = z.object({
+  name: z.string().min(1, "이름을 입력해주세요"),
+  phone: z.string().min(1, "전화번호를 입력해주세요"),
+  phoneVerification: z
+    .string()
+    .min(6, "6자리의 인증번호를 입력해주세요")
+    .max(6, "6자리의 인증번호를 입력해주세요"),
+})
+
+type FindEmailSchema = z.input<typeof findEmailSchema>
 const FindEmailPage = () => {
-  return <div>find email placeholder</div>
+  const postMutation = useMutation({
+    mutationFn: (body: FindEmailSchema) =>
+      // NOTE: api가 아직 나오지 않음
+      plainInstance.post("/accounts/find-email", body),
+  })
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(findEmailSchema) })
+
+  const onSubmit = (data: FindEmailSchema) => {
+    console.log({ data })
+    postMutation.mutate(data)
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Vstack gap="xl">
+        <NarrowTitleSection
+          title="이메일 찾기"
+          description="가입 시 등록한 정보를 입력해주세요"
+        />
+
+        <Labeled isError={Boolean(errors.name)}>
+          <Labeled.Title>이메일</Labeled.Title>
+          <Input
+            {...register("name")}
+            placeholder="이름을 입력해주세요"
+            status={errors.name ? "error" : "none"}
+          />
+          <Labeled.Message className="grow">
+            {errors.name?.message}
+          </Labeled.Message>
+        </Labeled>
+
+        <Labeled isError={Boolean(errors.phone)}>
+          <Labeled.Title>전화번호</Labeled.Title>
+          <Hstack gap="sm">
+            <Input
+              {...register("phone")}
+              status={errors.phone ? "error" : "none"}
+              type="number"
+              placeholder={`"-"없이 숫자만 입력해주세요`}
+              className="grow"
+            />
+            <Button type="button">인증</Button>
+          </Hstack>
+          <Labeled.Message>{errors.phone?.message}</Labeled.Message>
+        </Labeled>
+
+        <Labeled isError={Boolean(errors.phoneVerification)}>
+          <Labeled.Title>전화번호 인증코드</Labeled.Title>
+          <Input
+            {...register("phoneVerification")}
+            status={errors.phoneVerification ? "error" : "none"}
+            placeholder="6자리 코드를 입력해주세요"
+          />
+          <Labeled.Message>{errors.phoneVerification?.message}</Labeled.Message>
+        </Labeled>
+
+        <Button className="mt-lg">이메일 찾기</Button>
+      </Vstack>
+    </form>
+  )
 }
 
 export default FindEmailPage
