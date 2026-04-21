479d5b4d2ec2e1146a5b5be72010ce1208e1426a
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-04-21 16:13:35 +0900

    chore: schema field name를 api 명세서에 맞게 수정 (#151)

--- src/features/_narrow.signup/page/use-signup/use-signup.ts
+++ src/features/_narrow.signup/page/use-signup/use-signup.ts
@@ -3,14 +3,13 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useMutation } from "@tanstack/react-query"
 import { useForm } from "react-hook-form"
 import z from "zod"
-import useSignupStore from "../../store/use-signup-store"
 
 const signupSchema = z.object({
   email: z
     .string()
     .min(1, "이메일을 입력해주세요")
     .email("올바른 이메일 형식으로 입력해주세요"),
-  email_token: z
+  emailVerification: z
     .string()
     .min(6, "6자리의 인증번호를 입력해주세요")
     .max(6, "6자리의 인증번호를 입력해주세요"),
@@ -23,11 +22,11 @@ const signupSchema = z.object({
     ),
   name: z.string().min(1, "이름을 입력해주세요"),
   phone_number: z.string().min(1, "전화번호를 입력해주세요"),
-  phone_token: z
+  phoneVerification: z
     .string()
     .min(6, "6자리의 인증번호를 입력해주세요")
     .max(6, "6자리의 인증번호를 입력해주세요"),
-  birthday: z
+  birthYmd: z
     .string()
     .min(8, "8자리의 생년월일을 입력해주세요")
     .max(8, "8자리의 생년월일을 입력해주세요"),
@@ -36,37 +35,50 @@ const signupSchema = z.object({
 type SignupSchema = z.input<typeof signupSchema>
 
 const useSignup = () => {
-  const setModalKey = useSignupStore((state) => state.setModalKey)
   const { mutate } = useMutation({
     mutationFn: (body: SignupSchema) =>
       plainInstance.post("accounts/signup", body),
-    onSuccess: () => setModalKey("success"),
-    onError: () => setModalKey("error"),
   })
-
-  const useFormReturns = useForm({ resolver: zodResolver(signupSchema) })
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors },
-  } = useFormReturns
+  } = useForm({ resolver: zodResolver(signupSchema) })
 
   const onSubmit = (data: SignupSchema) => {
-    console.log({ data })
-    // mutate(data) // TODO: 회원가입 API에서 gender가 빠지면 이걸 사용합니다
-
-    // TODO: 회원가입 api 에서 gender가 빠지면 아래를 삭제합니다
-    const body = { ...data, gender: "M" }
-    mutate(body)
+    mutate(data)
   }
 
   const submitForm = handleSubmit(onSubmit)
+
+  // TODO: verification 로직 추후 작성되면 한 번 더 정리해야
+  const handleEmailVerification = async () => {
+    const email = watch().email
+    await plainInstance.post(
+      "https://fragmnt.pics/api/v1/accounts/verification/send-email",
+      {
+        email,
+      }
+    )
+  }
+
+  const handlePhoneVerification = async () => {
+    const phone_number = watch().phone_number
+    await plainInstance.post(
+      "https://fragmnt.pics/api/v1/accounts/verification/send-sms",
+      {
+        phone_number,
+      }
+    )
+  }
 
   return {
     register,
     submitForm,
     errors,
-    useFormReturns,
+    handleEmailVerification,
+    handlePhoneVerification,
   }
 }
 
