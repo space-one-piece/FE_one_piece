fbdbb6540bf9c5a0527840e4b7076ad6ee8e5b84
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-04-21 14:16:09 +0900

    feat: 스토어에 토큰 내용 추가 (#152)

--- src/shared/api/use-auth-store.ts
+++ src/shared/api/use-auth-store.ts
@@ -1,12 +1,32 @@
 import { create } from "zustand"
+import { createJSONStorage, persist } from "zustand/middleware"
+
 type AuthStoreState = {
-  access_token: string | null
-  setAccessToken: (access_token: string | null) => void
+  accessToken: string | null
+  setAccessToken: (accessToken: string | null) => void
+
+  refreshToken: string | null
+  setRefreshToken: (refreshToken: string | null) => void
 }
 
-const useAuthStore = create<AuthStoreState>()((set) => ({
-  access_token: null,
-  setAccessToken: (access_token) => set({ access_token }),
-}))
+const useAuthStore = create<AuthStoreState>()(
+  persist(
+    (set) => ({
+      accessToken: null,
+      setAccessToken: (accessToken) => set({ accessToken }),
+
+      refreshToken: null,
+      setRefreshToken: (refreshToken) => set({ refreshToken }),
+    }),
+    {
+      name: "fragmnt store",
+      storage: createJSONStorage(() => localStorage),
+      partialize: (state) => ({
+        accessToken: state.accessToken,
+        refreshToken: state.refreshToken,
+      }),
+    }
+  )
+)
 
 export default useAuthStore
