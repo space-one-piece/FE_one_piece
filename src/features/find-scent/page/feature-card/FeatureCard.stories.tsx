c89ceb7dbd8c656f295de06605256d098f1d37ef
Clacki <skthk6613@naver.com>
2026-04-22 13:31:52 +0900

    feat: 스토리북 수정 (#159)

--- src/features/find-scent/page/feature-card/FeatureCard.stories.tsx
+++ src/features/find-scent/page/feature-card/FeatureCard.stories.tsx
@@ -31,9 +31,9 @@ export const Photo: Story = {
     description:
       "사진 속 공간의 무드를 포착해\n분위기를 분석한 향기를 제안합니다",
     buttonLabel: "향기 찾기",
+    actionType: "navigate",
   },
 }
-
 export const Chat: Story = {
   args: {
     imageSrc: ChatImage,
@@ -41,9 +41,9 @@ export const Chat: Story = {
     title: "대화 기반 취향 분석",
     description: "당신의 문장 사이에서\n오늘의 향기를 찾습니다",
     buttonLabel: "향기 찾기",
+    actionType: "navigate",
   },
 }
-
 export const Keyword: Story = {
   args: {
     imageSrc: SurveyImage,
@@ -51,9 +51,9 @@ export const Keyword: Story = {
     title: "키워드 기반 정밀 분석",
     description: "선호하는 분위기와 향 키워드를\n조합하여 향기를 찾습니다",
     buttonLabel: "향기 찾기",
+    actionType: "modal",
   },
 }
-
 export const Default: Story = {
   args: {
     imageSrc: PhotoImage,
@@ -62,5 +62,6 @@ export const Default: Story = {
     description:
       "사진 속 공간의 무드를 포착해\n분위기를 분석한 향기를 제안합니다",
     buttonLabel: "향기 찾기",
+    actionType: "navigate",
   },
 }
