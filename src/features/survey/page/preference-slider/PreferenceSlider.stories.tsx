ae67a9860d0255c4940c9654e7d1fc076700de84
Clacki <skthk6613@naver.com>
2026-04-23 15:14:30 +0900

    fix: 스토리북 수정 (#170)

--- src/features/survey/page/preference-slider/PreferenceSlider.stories.tsx
+++ src/features/survey/page/preference-slider/PreferenceSlider.stories.tsx
@@ -20,6 +20,12 @@ const meta = {
     item: {
       control: "object",
     },
+    order: {
+      control: {
+        type: "number",
+        min: 1,
+      },
+    },
     value: {
       control: {
         type: "range",
@@ -43,20 +49,32 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
   args: {
+    order: 1,
     item: {
-      order: 1,
       title: "아침에 선호하는 향",
-      description: "하루를 시작할 때 어떤 느낌의 향을 원하시나요?",
-      labels: [
-        "매우 상쾌함",
-        "약간 상쾌함",
-        "보통",
-        "약간 포근함",
-        "매우 포근함",
+      additional: "하루를 시작할 때 어떤 느낌의 향을 원하시나요?",
+      left_label: "상쾌한",
+      right_label: "포근한",
+      answer: [
+        {
+          content: "매우 상쾌함",
+        },
+        {
+          content: "약간 상쾌함",
+        },
+        {
+          content: "보통",
+        },
+        {
+          content: "약간 포근함",
+        },
+        {
+          content: "매우 포근함",
+        },
       ],
-      edgeLabels: ["상쾌한", "포근한"],
     },
     value: 2,
+    onChange: () => {},
   },
   render: function Render(args) {
     const [{ value }, updateArgs] = useArgs()
