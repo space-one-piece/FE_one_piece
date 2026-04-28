9239de09bf049325971e74a8a47620b11c46d788
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-04-28 15:13:41 +0900

    fix: 쿠키 사용해 jwt 재발급 요청 보내게 수정 (#219)

--- src/shared/api/use-auth-store.ts
+++ src/shared/api/use-auth-store.ts
@@ -6,9 +6,6 @@ type AuthStoreState = {
   accessToken: string | null
   setAccessToken: (accessToken: string | null) => void
 
-  refreshToken: string | null
-  setRefreshToken: (refreshToken: string | null) => void
-
   profile: Profile | null
   setProfile: (profile: Profile | null) => void
 
@@ -21,16 +18,13 @@ const useAuthStore = create<AuthStoreState>()(
       accessToken: null,
       setAccessToken: (accessToken) => set({ accessToken }),
 
-      refreshToken: null,
-      setRefreshToken: (refreshToken) => set({ refreshToken }),
-
       profile: null,
       setProfile: (profile) => set({ profile }),
 
       logout: () => {
         // TODO: 로그아웃 요청도 보내야 합니다
         // TODO: 추후 profile을 저장하면 그것도 지워야 합니다
-        set({ accessToken: null, refreshToken: null })
+        set({ accessToken: null })
       },
     }),
     {
@@ -38,7 +32,6 @@ const useAuthStore = create<AuthStoreState>()(
       storage: createJSONStorage(() => localStorage),
       partialize: (state) => ({
         accessToken: state.accessToken,
-        refreshToken: state.refreshToken,
       }),
     }
   )
