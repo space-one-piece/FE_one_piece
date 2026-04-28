3cfcbd84e53377a5ab498d33ac962a2de2c6fbe8
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-04-28 14:26:23 +0900

    chore: 죽은 코드 정리 (#200)

--- src/features/_narrow.signup/page/use-signup/use-signup.ts
+++ src/features/_narrow.signup/page/use-signup/use-signup.ts
@@ -48,7 +48,6 @@ const useSignup = () => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useFormReturns
 
@@ -63,57 +62,10 @@ const useSignup = () => {
 
   const submitForm = handleSubmit(onSubmit)
 
-  const handleEmailVerificationFirst = async () => {
-    const email = watch().email
-    await plainInstance.post(
-      "https://fragmnt.pics/api/v1/accounts/verification/send-email",
-      {
-        email,
-      }
-    )
-  }
-  const handleEmailVerificationSecond = async () => {
-    const email = watch().email
-    const email_token = watch().email_token
-    await plainInstance.post(
-      "https://fragmnt.pics/api/v1/accounts/verification/verify-email",
-      {
-        email,
-        code: email_token,
-      }
-    )
-  }
-
-  const handlePhoneVerificationFirst = async () => {
-    const phone_number = watch().phone_number
-    await plainInstance.post(
-      "https://fragmnt.pics/api/v1/accounts/verification/send-sms",
-      {
-        phone_number,
-      }
-    )
-  }
-
-  const handlePhoneVerificationSecond = async () => {
-    const phone_number = watch().phone_number
-    const phone_token = watch().phone_token
-    await plainInstance.post(
-      "https://fragmnt.pics/api/v1/accounts/verification/verify-sms",
-      {
-        phone_number,
-        code: phone_token,
-      }
-    )
-  }
-
   return {
     register,
     submitForm,
     errors,
-    handleEmailVerificationFirst,
-    handleEmailVerificationSecond,
-    handlePhoneVerificationFirst,
-    handlePhoneVerificationSecond,
     useFormReturns,
   }
 }
