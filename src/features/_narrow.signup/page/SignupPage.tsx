727a04af55883ca2d4cb4999f1e6026bfe861ce4
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-04-29 15:23:58 +0900

    Revert "feat: request body field name을 api 명세서에 맞게 수정 (#151)"
    This reverts commit 8b243cb28378a0d9228e33fb8c8f2e81c33b320a.

--- src/features/_narrow.signup/page/SignupPage.tsx
+++ src/features/_narrow.signup/page/SignupPage.tsx
@@ -36,14 +36,14 @@ const SignupPage = () => {
           <Labeled.Message>{errors.email?.message}</Labeled.Message>
         </Labeled>
 
-        <Labeled isError={Boolean(errors.email_token)}>
+        <Labeled isError={Boolean(errors.emailVerification)}>
           <Labeled.Title>이메일 인증코드</Labeled.Title>
           <Input
-            {...register("email_token")}
-            status={errors.email_token ? "error" : "none"}
+            {...register("emailVerification")}
+            status={errors.emailVerification ? "error" : "none"}
             placeholder="6자리 코드를 입력해주세요"
           />
-          <Labeled.Message>{errors.email_token?.message}</Labeled.Message>
+          <Labeled.Message>{errors.emailVerification?.message}</Labeled.Message>
         </Labeled>
 
         <Labeled isError={Boolean(errors.password)}>
@@ -83,25 +83,25 @@ const SignupPage = () => {
           <Labeled.Message>{errors.phone_number?.message}</Labeled.Message>
         </Labeled>
 
-        <Labeled isError={Boolean(errors.phone_token)}>
+        <Labeled isError={Boolean(errors.phoneVerification)}>
           <Labeled.Title>전화번호 인증코드</Labeled.Title>
           <Input
-            {...register("phone_token")}
-            status={errors.phone_token ? "error" : "none"}
+            {...register("phoneVerification")}
+            status={errors.phoneVerification ? "error" : "none"}
             placeholder="6자리 코드를 입력해주세요"
           />
-          <Labeled.Message>{errors.phone_token?.message}</Labeled.Message>
+          <Labeled.Message>{errors.phoneVerification?.message}</Labeled.Message>
         </Labeled>
 
-        <Labeled isError={Boolean(errors.birthday)}>
+        <Labeled isError={Boolean(errors.birthYmd)}>
           <Labeled.Title>생년월일</Labeled.Title>
           <Input
-            {...register("birthday")}
+            {...register("birthYmd")}
             type="number"
-            status={errors.birthday ? "error" : "none"}
+            status={errors.birthYmd ? "error" : "none"}
             placeholder="8자리 숫자 입력 (예: 19980514)"
           />
-          <Labeled.Message>{errors.birthday?.message}</Labeled.Message>
+          <Labeled.Message>{errors.birthYmd?.message}</Labeled.Message>
         </Labeled>
         <Button className="mt-lg">회원가입</Button>
       </Vstack>
