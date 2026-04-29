a791e8f10cee47305debcc597b3647e36dc4feac
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-04-29 15:47:50 +0900

    feat: uuid 토큰 타입 추가 (#235)

--- src/shared/components/verification-fields/types/use-form-return.type.ts
+++ src/shared/components/verification-fields/types/use-form-return.type.ts
@@ -1,2 +1,10 @@
-export type EmailFields = { email?: string; email_token?: string }
-export type PhoneFields = { phone_number?: string; phone_token?: string }
+export type EmailFields = {
+  email?: string
+  email_token?: string
+  email_uuid_token?: string
+}
+export type PhoneFields = {
+  phone_number?: string
+  phone_token?: string
+  sms_uuid_token?: string
+}
