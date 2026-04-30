df8371694cf025cb7502e41786518a62d0ea5179
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-04-30 16:11:06 +0900

    fix: login search validation 단순화 (#204)

--- src/features/_wide.index/utils/handle-scroll/handle-scroll.ts
+++ src/features/_wide.index/utils/handle-scroll/handle-scroll.ts
@@ -1,5 +1,7 @@
 export const handleScroll = (selector: string) => {
   const quickStartDiv = document.querySelector(selector)
+  if (!quickStartDiv) return
+
   quickStartDiv.scrollIntoView({
     behavior: "smooth",
     block: "center",
--- src/routes/_narrow.login.tsx
+++ src/routes/_narrow.login.tsx
@@ -1,11 +1,12 @@
 import LoginPage from "@/features/_narrow.login/page/LoginPage"
 import { createFileRoute } from "@tanstack/react-router"
+import z from "zod"
+
+const validateSearch = z.object({
+  reason: z.string().optional(),
+})
 
 export const Route = createFileRoute("/_narrow/login")({
-  validateSearch: (search: Record<string, unknown>) => {
-    return {
-      reason: typeof search.reason === "string" ? search.reason : undefined,
-    }
-  },
+  validateSearch,
   component: LoginPage,
 })
