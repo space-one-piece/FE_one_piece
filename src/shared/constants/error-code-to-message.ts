58f90d35716cf229d88d387386a41650d3d3dadb
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-05-06 13:38:27 +0900

    chore: 매핑된 에러 메시지 수정 (#264)

--- src/shared/constants/error-code-to-message.ts
+++ src/shared/constants/error-code-to-message.ts
@@ -1,5 +1,5 @@
 export const ERROR_CODE_TO_MESSAGE = {
-  USER_ALREADY_EXISTS: "이미 가입되었습니다",
+  USER_ALREADY_EXISTS: "이미 가입된 이메일 혹은 전화번호입니다",
   INVALID_EMAIL_FORMAT: "이메일 형식이 올바르지 않습니다",
   INVALID_PHONE_FORMAT: "전화번호 형식이 올바르지 않습니다",
   VALIDATION_ERROR: "입력값이 유효하지 않습니다", //NOTE:	일반적인 필드 검증 실패
