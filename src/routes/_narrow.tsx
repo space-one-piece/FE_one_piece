a5ca58b36e8332d9cc5349dad5d3ff1bf5e17d12
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-05-07 14:46:34 +0900

    feat: narrow 라우트에 로그인하고 접근하면 메인 페이지로 이동 (#299)

--- src/routes/_narrow.tsx
+++ src/routes/_narrow.tsx
@@ -1,6 +1,16 @@
 import NarrowLayout from "@/features/_narrow/layout/NarrowLayout"
-import { createFileRoute } from "@tanstack/react-router"
+import useAuthStore from "@/shared/api/use-auth-store"
+import { createFileRoute, redirect } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/_narrow")({
   component: NarrowLayout,
+  beforeLoad: () => {
+    const accessToken = useAuthStore.getState().accessToken
+    if (!accessToken) return
+
+    throw redirect({
+      to: "/",
+      replace: true,
+    })
+  },
 })
